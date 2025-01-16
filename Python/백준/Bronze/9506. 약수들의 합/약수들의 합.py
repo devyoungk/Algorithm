155fc299de213f6a4827ff54989c33cc8752fcd8
morelight <167386812+devyoungk@users.noreply.github.com>
2025-01-16 21:38:48 +0900

    [Bronze I] Title: 약수들의 합, Time: 164 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -0,0 +1,16 @@
+while True:
+    n = int(input())
+    if n == -1: 
+        break
+    A = []
+    for i in range(2,n):
+        if n%i == 0:
+            A.append(i)
+        if sum(A)+1 == n:
+            B = ''
+            B += str(n) + ' = 1'
+            for a in A:
+                B += ' + ' + str(a)
+        else:
+            B = str(n) + ' is NOT perfect.'
+    print(B)
