93d9c7691b88118b3fae6c060af38cdda82820a6
morelight <167386812+devyoungk@users.noreply.github.com>
2025-01-21 12:15:19 +0900

    [Silver V] Title: 덩치, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/7568. 덩치/덩치.py
+++ Python/백준/Silver/7568. 덩치/덩치.py
@@ -3,12 +3,12 @@ A = []
 for i in range(n):
     x, y = input().split()
     A.append([int(x), int(y)])
+B = []
 for i in range(n):
     rank = 1
     for j in range(n):
         if A[i][0] < A[j][0] and A[i][1] < A[j][1]:
             rank += 1
-    A[i].append(rank)
+    B.append(rank)
 
-for i in range(n):
-    print(A[i][2], end=' ')
+print(*B)
