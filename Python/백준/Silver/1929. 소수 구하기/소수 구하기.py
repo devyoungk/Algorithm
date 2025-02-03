80398f672170c8c1bc461b5d18934a05414a407c
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-03 16:25:04 +0900

    [Silver III] Title: 소수 구하기, Time: 168 ms, Memory: 42624 KB -BaekjoonHub

--- Python/백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ Python/백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,12 +1,13 @@
-def isprime(n):
-    if n < 2:
-        return False
+def primelist(m, n):  # m 이상 n 이하의 소수를 담은 리스트 정의
+    A = [True] * (n+1) # index n까지
+    A[0] = A[1] = False  # 0과 1은 소수가 아님
     for i in range(2,int(n**0.5)+1):
-        if n%i == 0:
-            return False
-    return True
+    	if A[i] : # i가 소수일 때
+            for j in range(i*i, n+1, i): # i의 배수 제외하는 과정정
+                A[j] = False  # 소수가 아님을 나타낸다.
+    return [i for i in range(m, n+1) if A[i]]  # 소수만 포함하는 리스트
 
 a, b = map(int,input().split())
-for i in range(a, b+1):
-    if isprime(i):
-        print(i)
+primes = primelist(a, b)
+for n in primes:
+    print(n)
