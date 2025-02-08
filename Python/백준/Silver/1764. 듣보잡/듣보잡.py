9d4977052be2e7dce3c17028de949ef245dee291
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-08 22:09:38 +0900

    [Silver IV] Title: 듣보잡, Time: 72 ms, Memory: 38048 KB -BaekjoonHub

---
+++ Python/백준/Silver/1764. 듣보잡/듣보잡.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int,input().split())
+A = []
+for _ in range(N):
+    s = input().strip()
+    A.append(s)
+A = set(A)
+B = []
+for _ in range(M):
+    s = input().strip()
+    if s in A:
+        B.append(s)
+B.sort()
+print(len(B))
+print("\n".join(B))
