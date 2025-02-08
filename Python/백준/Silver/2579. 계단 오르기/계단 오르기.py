3667fc42cb3c28236e5c0a786cd429d9ac4751b8
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-08 23:03:34 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -0,0 +1,19 @@
+N = int(input())
+A = [int(input()) for _ in range(N)]
+
+score = [0]*N
+score[0] = A[0]
+if N == 1:
+    print(score[0])
+    exit()
+score[1] = A[0] + A[1]
+if N == 2:
+    print(score[1])
+    exit()
+    
+score[2] = max(A[0] + A[2], A[1]+A[2])
+
+for i in range(3, N):
+    score[i] = max(score[i-2] + A[i], score[i-3] + A[i-1] + A[i])
+
+print(score[N-1])
