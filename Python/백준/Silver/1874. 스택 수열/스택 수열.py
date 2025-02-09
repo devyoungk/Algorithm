cbdb1c5b4ad22f5a0ab525749d1647fd8f084b3d
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-09 22:26:07 +0900

    [Silver II] Title: 스택 수열, Time: 2600 ms, Memory: 42884 KB -BaekjoonHub

---
+++ Python/백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,27 @@
+N = int(input())
+A = []
+B = [int(input()) for _ in range(N)]
+C = []
+D = []
+
+i = 0
+j = 1
+while j <= N:
+    if j <= B[i]:
+        C.append(j)
+        A.append("+")
+        j += 1
+    elif C:
+        D.append(C[-1]) 
+        C.pop(-1)
+        A.append("-")
+        i += 1
+
+E = D + C[::-1]
+A += ["-"]*len(C)
+
+
+if E != B:
+    print("NO")
+else:
+    print("\n".join(A))
