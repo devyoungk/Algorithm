0a45bd21027ca7e362e7aa4c799a9128090b67e1
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-11 21:23:55 +0900

    [Silver III] Title: 1로 만들기, Time: 424 ms, Memory: 40224 KB -BaekjoonHub

--- Python/백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ Python/백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,17 +1,7 @@
 n = int(input())
 A = [0]*(n+1)
-if n == 1:
-    print(A[n])
-    exit()
-A[2] = 1
-if n == 2:
-    print(A[n])
-    exit()
-A[3] = 1
-if n == 3:
-    print(A[n])
-    exit()
-for i in range(4,n+1):
+
+for i in range(2,n+1):
     if i%6 == 0:
         A[i] = 1 + min(A[i//3], A[i//2], A[i-1])
     elif i%3 == 0:
