8f910a3717492fb83275dd2f43b9ddb95a086bb4
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-17 23:01:48 +0900

    [Silver III] Title: Four Squares, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/17626. Four Squares/Four Squares.py
+++ Python/백준/Silver/17626. Four Squares/Four Squares.py
@@ -4,7 +4,7 @@ if int(n**0.5) == n**0.5:
     print(1)
     exit()
 
-A = [i ** 2 for i in range(1, int(n**0.5) + 1)]
+A = {i ** 2 for i in range(1, int(n**0.5) + 1)}
 
 for a in A:
     if n-a in A:
@@ -14,7 +14,7 @@ for a in A:
 for a in A:
     for b in A:
         if a + b > n:
-            break
+            continue
         if n-a-b in A:
             print(3)
             exit()
