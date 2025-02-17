856e77bb4cce8bbb8843bb232d80c7400ebc5582
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-17 22:53:41 +0900

    [Silver III] Title: Four Squares, Time: 136 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/17626. Four Squares/Four Squares.py
+++ Python/백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,16 +1,15 @@
-import sys
-n = int(sys.stdin.readline().strip())
+n = int(input())
 
 if int(n**0.5) == n**0.5:
     print(1)
-    sys.exit()
+    exit()
 
 A = [i ** 2 for i in range(1, int(n**0.5) + 1)]
 
 for a in A:
     if n-a in A:
         print(2)
-        sys.exit()
+        exit()
 
 for a in A:
     for b in A:
@@ -18,6 +17,6 @@ for a in A:
             break
         if n-a-b in A:
             print(3)
-            sys.exit()
+            exit()
 
 print(4)
