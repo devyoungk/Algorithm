9eaac133aea301cb94cff6e76fbbb1dcef249d33
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-19 12:45:00 +0900

    [Silver III] Title: 게임, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1072. 게임/게임.py
+++ Python/백준/Silver/1072. 게임/게임.py
@@ -1,10 +1,19 @@
-import math
-
 X, Y = map(int,input().split())
 w = Y*100//X 
+
 if w >= 99:
     print(-1)
     exit()
 
-else:
-    print(math.ceil((X*w+X-100*Y)/(99-w)))
+l = 0
+r = X
+a = -1
+
+while l <= r:
+    m = (l+r)//2
+    if int((Y+m)*100//(X+m)) > w:
+        a = m
+        r = m-1
+    else:
+        l = m+1
+print(a)
