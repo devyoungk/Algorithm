4a6e0567b7a7d85966b48654755ff20c20d07d22
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-21 23:08:09 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.41 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -0,0 +1,8 @@
+def solution(left, right):
+    answer = 0
+    for n in range(left, right+1):
+        if n**0.5 == int(n**0.5):
+            answer -= n
+        else:
+            answer += n
+    return answer
