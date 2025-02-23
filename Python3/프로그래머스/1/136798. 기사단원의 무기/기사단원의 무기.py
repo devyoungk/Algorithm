2e4001402f4651934057714adcf59100cd2ac707
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-23 15:29:11 +0900

    [level 1] Title: 기사단원의 무기, Time: 1225.09 ms, Memory: 10.5 MB -BaekjoonHub

--- Python3/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ Python3/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,12 +1,13 @@
 def solution(number, limit, power):
-    answer = 0
-    for i in range(1, number+1):
-        A = []
-        for j in range(1, int(i**0.5)+1):
+    answer = 1
+    for i in range(2, number+1):
+        s = 1
+        for j in range(2, int(i**0.5)+1):
             if i%j == 0:
-                A.append(j)
-                A.append(i//j)         
-        s = len(set(A))
+                s += 1
+        s *= 2
+        if int(i**0.5) == i**0.5:
+            s -= 1
         if s > limit:
             answer += power
         else:    
