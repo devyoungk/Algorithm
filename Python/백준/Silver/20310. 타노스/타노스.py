3f589155a8c33188cc0506f32d07971be0bc70c6
morelight <167386812+devyoungk@users.noreply.github.com>
2025-02-26 22:49:54 +0900

    [Silver III] Title: 타노스, Time: 44 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- Python/백준/Silver/20310. 타노스/타노스.py
+++ Python/백준/Silver/20310. 타노스/타노스.py
@@ -1,3 +1,27 @@
 S = list(input())
 
-print('0' * (S.count('0')//2) + '1' * ((len(S)-S.count('0'))//2))
+a = S.count('0')
+b = len(S) - a
+
+answer = ''
+c1 = 0
+c2 = 0
+
+for i in range(len(S)):
+    if S[i] == '1':
+        c2 += 1
+        S[i] = 'C'
+    if c2 == b//2:
+        break
+
+for i in range(len(S)-1, -1, -1):
+    if S[i] == '0':
+        c1 += 1
+        S[i] = 'C'
+    if c1 == a//2:
+        break
+
+for s in S:
+    if s in ('0', '1'):
+        answer += s
+print(answer)
