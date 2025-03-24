ab9699cbcb13e046f2114a78b1c04a7a250f4c47
morelight <167386812+devyoungk@users.noreply.github.com>
2025-03-24 12:29:19 +0900

    [Silver IV] Title: 대칭 차집합, Time: 196 ms, Memory: 85160 KB -BaekjoonHub

--- Python/백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ Python/백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -2,8 +2,4 @@ N, M = map(int,input().split())
 A = set(input().split())
 B = set(input().split())
 
-cnt = 0
-for a in A:
-    if a in B:
-        cnt += 1
-print(N+M-2*cnt)
+print(N+M-2*len(A.intersection(B)))
