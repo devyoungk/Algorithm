cd83c282a0c48eb08db3f88b245a815a588f6307
morelight <167386812+devyoungk@users.noreply.github.com>
2025-03-30 18:03:49 +0900

    [Silver IV] Title: 가로수, Time: 2552 ms, Memory: 38392 KB -BaekjoonHub

---
+++ Python/백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,16 @@
+import math
+
+N = int(input())
+A = []
+for _ in range(N):
+    A.append(int(input()))
+B = set()
+for i in range(N-1,0,-1):
+    B.add(A[i]-A[i-1])
+
+B = list(B)
+x = B[0]
+for i in range(1, len(B)):
+    x = math.gcd(x,B[i])
+
+print((A[-1]-A[0])//x + 1 - N)
