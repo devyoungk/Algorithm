3f1a0a9c2bfe200ee6e8d8a6f5bd3756a034b634
morelight <167386812+devyoungk@users.noreply.github.com>
2025-04-12 13:58:45 +0900

    [Silver II] Title: 색종이 만들기, Time: 52 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -0,0 +1,47 @@
+def check(A):
+    n = len(A)
+    b = 0
+    w = 0
+    for i in range(n):
+        b += A[i].count('1')
+        w += A[i].count('0')
+        if w > 0 and b > 0:
+            return 0
+    if w == 0:
+        return 'blue'
+    elif b == 0:
+        return 'white'
+    else:
+        return 0
+
+def split(A):
+    n = len(A)
+    mid = n//2
+
+    A1 = [a[:mid] for a in A[:mid]]
+    A2 = [a[mid:] for a in A[:mid]]
+    A3 = [a[:mid] for a in A[mid:]]
+    A4 = [a[mid:] for a in A[mid:]]
+
+    return [A1, A2, A3, A4]
+
+N = int(input())
+A = [list(input().split()) for _ in range(N)]
+
+blue = 0
+white = 0
+def sol(A):
+    global blue, white
+    result = check(A)
+    if result == 'blue':
+        blue += 1
+    elif result == 'white':
+        white += 1
+    else:
+        for a in split(A):
+            sol(a)
+
+sol(A)
+
+print(white)
+print(blue)
