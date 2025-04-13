b97807ac1ab5049dc1d84fa58b84efe8567cd744
morelight <167386812+devyoungk@users.noreply.github.com>
2025-04-13 18:38:10 +0900

    [level 2] Title: 롤케이크 자르기, Time: 476.37 ms, Memory: 56.3 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,25 @@
+def solution(topping):
+    answer = 0
+    A = {}
+    for t in topping:
+        if t in A:
+            A[t] += 1
+        else:
+            A[t] = 1
+    NL = len(set(topping))
+    NR = 0
+    B = set()
+    for t in topping[::-1]:
+        if t not in B:
+            B.add(t)
+            NR += 1
+        if t in A:
+            A[t] -= 1
+            if A[t] == 0:
+                del A[t]
+                NL -= 1
+        if NL == NR:
+            answer += 1
+        if NL < NR:
+            break
+    return answer
