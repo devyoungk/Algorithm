0fdc40fcb5bbed71222655cedf4fd5c8caa337b3
morelight <167386812+devyoungk@users.noreply.github.com>
2025-04-14 20:05:33 +0900

    [Silver II] Title: 좌표 압축, Time: 1416 ms, Memory: 186660 KB -BaekjoonHub

--- Python/백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ Python/백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,11 +1,10 @@
 N = int(input())
 A = list(map(int,input().split()))
-B = sorted(A)
+B = sorted(set(A))
 C = {}
 i = 0
 for b in B:
-    if b not in C:
-        C[b] = i
-        i += 1
-D = [C[a] for a in A]
-print(' '.join(map(str,D)))
+    C[b] = i
+    i += 1
+
+print(' '.join(str(C[a]) for a in A))
