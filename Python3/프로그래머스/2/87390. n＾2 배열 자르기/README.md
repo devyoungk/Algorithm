6edcaede5e1d9dd2938b16040913162ff5a44762
morelight <167386812+devyoungk@users.noreply.github.com>
2025-04-22 18:14:48 +0900

    [level 2] Title: n^2 배열 자르기, Time: 49.57 ms, Memory: 17.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/87390. n＾2 배열 자르기/README.md
+++ Python3/프로그래머스/2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390#) 
 
 ### 성능 요약
 
-메모리: 17.5 MB, 시간: 1247.47 ms
+메모리: 17.5 MB, 시간: 49.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 22일 18:12:13
+2025년 04월 22일 18:14:38
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ Python3/프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,14 +1,5 @@
 def solution(n, left, right):
     answer = []
-    for i in range(left//n, right//n+1):
-        for j in range(n):
-            if i == left//n:
-                if j < left%n:
-                    continue
-            x = max(i,j) + 1
-            answer.append(x)
-            if i == right//n and j == right%n:
-                break
-            
-    
+    for i in range(left, right+1):
+        answer.append(max(i//n, i%n)+1)  
     return answer
