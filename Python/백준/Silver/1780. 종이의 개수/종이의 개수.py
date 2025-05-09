aa85442a6a18c06ef9dbd44e7cf521e615b44d1c
morelight <167386812+devyoungk@users.noreply.github.com>
2025-05-09 12:01:48 +0900

    [Silver II] Title: 종이의 개수, Time: 7440 ms, Memory: 113396 KB -BaekjoonHub

--- Python/백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ Python/백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -11,23 +11,12 @@ def divide(A):
 
 def cnt(A):
     N = len(A)
-    a, b, c = 0, 0, 0
+    X = A[0][0]
     for i in range(N):
         for j in range(N):
-            if A[i][j] == -1:
-                a += 1
-            elif A[i][j] == 0:
-                b += 1
-            else:
-                c += 1
-            if a*b or b*c or c*a:
+            if A[i][j] != X:
                 return -1
-    if a:
-        return '-1'
-    elif b:
-        return '0'
-    else:
-        return '1'
+    return str(X)
 
 x, y, z = 0, 0, 0    
 
