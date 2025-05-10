369113e63bb5fa229ca4336d109ae86f7d1af093
morelight <167386812+devyoungk@users.noreply.github.com>
2025-05-10 14:25:29 +0900

    [Silver II] Title: 베르트랑 공준, Time: 4424 ms, Memory: 34416 KB -BaekjoonHub

--- Python/백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ Python/백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,19 +1,19 @@
 def isPrime(N):
     for i in range(2, int(N**0.5)+1):
         if N%i == 0:
-            return False
-    return True
+            return 0
+    return 1
 
-P = set()
+A = [0 for _ in range(2*123456+1)]
 while True:
     N = int(input())
     if N == 0:
         break
     cnt = 0
     for i in range(N+1, 2*N+1):
-        if i in P:
+        if A[i]:
             cnt += 1
         elif isPrime(i):
             cnt += 1
-            P.add(i)
+            A[i] = 1
     print(cnt)
