ffce30c6370c1f75dbb179b6c4a25c00564fe7d4
morelight <167386812+devyoungk@users.noreply.github.com>
2025-05-10 15:55:34 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 2668 ms, Memory: 40660 KB -BaekjoonHub

--- Python/백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ Python/백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -6,13 +6,11 @@ for i in range(2, len(A)):
         for x in range(2*i, len(A), i):
             A[x] = 0
 
-B = set(i for i in range(len(A)) if A[i])
-
 T = int(input())
 for _ in range(T):
     N = int(input())
     cnt = 0
     for i in range(2, N//2+1):
-        if i in B and (N-i) in B:
+        if A[i] and A[N-i]:
             cnt += 1
     print(cnt)
