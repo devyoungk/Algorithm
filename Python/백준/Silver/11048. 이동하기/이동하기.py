f1210103af461a8efbad344537f5896d2ff86f51
morelight <167386812+devyoungk@users.noreply.github.com>
2025-05-17 23:12:31 +0900

    [Silver II] Title: 이동하기, Time: 536 ms, Memory: 71668 KB -BaekjoonHub

--- Python/백준/Silver/11048. 이동하기/이동하기.py
+++ Python/백준/Silver/11048. 이동하기/이동하기.py
@@ -1,15 +1,11 @@
 N, M = map(int,input().split())
-A = []
-for _ in range(N):
-    A.append(list(map(int,input().split())))
-for i in range(N):
-    for j in range(M):
-        if i == 0 and j == 0:
-            continue
-        elif i == 0:
-            A[i][j] += A[i][j-1]
-        elif j == 0:
-            A[i][j] += A[i-1][j]
-        else:
-            A[i][j] += max(A[i][j-1], A[i-1][j])
+A = [list(map(int,input().split())) for _ in range(N)]
+
+for i in range(1, N):
+    A[i][0] += A[i-1][0]
+for j in range(1, M):
+    A[0][j] += A[0][j-1]
+for i in range(1, N):
+    for j in range(1, M):
+        A[i][j] += max(A[i-1][j], A[i][j-1])
 print(A[-1][-1])
