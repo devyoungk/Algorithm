382723f932ab7a0e6a7b30df4812460209d8b05b
morelight <167386812+devyoungk@users.noreply.github.com>
2025-05-18 23:13:59 +0900

    [Silver III] Title: 01타일, Time: 244 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/1904. 01타일/01타일.py
+++ Python/백준/Silver/1904. 01타일/01타일.py
@@ -1,10 +1,11 @@
 N = int(input())
-dp = [0 for _ in range(N+1)]
-dp[1] = 1
-if N > 1:
-    dp[2] = 2
+if N == 1:
+    print(1)
+elif N == 2:
+    print(2)
 
-for i in range(3,N+1):
-    dp[i] = (dp[i-1]+dp[i-2])%15746
-
-print(dp[-1])
+else:
+    a, b = 1, 2
+    for _ in range(3,N+1):
+        a, b = b, (a+b)%15746
+    print(b)
