d42d30b73c52e29c64d87821e844494102c67751
morelight <167386812+devyoungk@users.noreply.github.com>
2025-05-19 23:26:34 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 512 ms, Memory: 71968 KB -BaekjoonHub

---
+++ Python/백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,15 @@
+T = int(input())
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+L = 3
+for _ in range(T):
+    N = int(input())
+    if dp[N]:
+        print(dp[N])
+    else:
+        for i in range(L+1, N+1):
+            dp[i] = sum(dp[i-3:i])%1000000009
+        print(dp[N])
+        L = N
