7dc71957b5a79dd78080e9ed6bf99c2564262718
morelight <167386812+devyoungk@users.noreply.github.com>
2025-06-02 15:36:53 +0900

    [Silver II] Title: 마인크래프트, Time: 152 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -0,0 +1,36 @@
+N, M, B = map(int,input().split())
+D = dict()
+for _ in range(N):
+    A = list(map(int,input().split()))
+    for a in A:
+        if a in D:
+            D[a] += 1
+        else:
+            D[a] = 1
+
+s = 0
+for d in D:
+    s += D[d]*d
+
+time = float('inf')
+height = 0
+targetH = max(D)
+
+while targetH >= 0:
+    Block_now = B
+    t = 0
+    for d in D:
+        hd = targetH - d
+        if hd > 0:
+            t += hd*D[d]
+            Block_now -= hd*D[d]
+        elif hd < 0:
+            Block_now -= hd*D[d]
+            t -= 2*hd*D[d]
+    if Block_now >= 0:
+        if t < time:
+            time = t
+            height = targetH
+    targetH -= 1
+
+print(time, height)
