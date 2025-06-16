ed5a23684c27ccacbfc3950f087eb666f88a0d58
morelight <167386812+devyoungk@users.noreply.github.com>
2025-06-16 22:40:24 +0900

    [Silver I] Title: 추월, Time: 176 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/2002. 추월/추월.py
+++ Python/백준/Silver/2002. 추월/추월.py
@@ -1,12 +1,12 @@
 N = int(input())
 In = [input() for _ in range(N)]
-Out = [input() for _ in range(N)]
+Out = {input():i for i in range(N)}
 
 cnt = 0
 
-for i in range(1,N):
+for i in range(N):
     for j in range(i):
-        if Out.index(In[i]) < Out.index(In[j]):
+        if Out[In[i]] < Out[In[j]]:
             cnt += 1
             break
 
