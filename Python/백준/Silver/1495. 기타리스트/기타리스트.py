113f447cafc5419db2b2c10650181343ebae8be0
morelight <167386812+devyoungk@users.noreply.github.com>
2025-06-22 22:28:24 +0900

    [Silver I] Title: 기타리스트, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1495. 기타리스트/기타리스트.py
@@ -0,0 +1,16 @@
+N, S, M = map(int,input().split())
+V = list(map(int,input().split()))
+
+now = [S]
+for i in range(N):
+    next = set()
+    for n in now:
+        if n+V[i] <= M:
+            next.add(n+V[i])
+        if n-V[i] >= 0:
+            next.add(n-V[i])
+    if not next:
+        break
+    now = list(next)
+
+print(max(next)) if next else print(-1)
