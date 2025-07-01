6b27d618991631b5dd47016ee8a287b06e53923a
morelight <167386812+devyoungk@users.noreply.github.com>
2025-07-01 20:36:21 +0900

    [Bronze V] Title: 고양이, Time: 100 ms, Memory: 13876 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
