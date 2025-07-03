0d88451cd3990e19d8898d83c2f35c24e4014228
morelight <167386812+devyoungk@users.noreply.github.com>
2025-07-03 15:58:36 +0900

    [Bronze V] Title: A×B, Time: 176 ms, Memory: 17712 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        System.out.println(a*b);
+    }
+}
