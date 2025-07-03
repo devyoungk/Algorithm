00d89db2bc64d8e3e9862a7ddc888b62d6fea059
morelight <167386812+devyoungk@users.noreply.github.com>
2025-07-03 16:03:42 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17592 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        char a = in.next().charAt(0);
+        int n = (int)a;
+
+        System.out.println(n);
+    }
+}
