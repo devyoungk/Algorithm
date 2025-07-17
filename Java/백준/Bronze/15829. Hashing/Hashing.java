da32f5ac441f798e400538e972e8d6b4cc20fa98
morelight <167386812+devyoungk@users.noreply.github.com>
2025-07-17 22:29:26 +0900

    [Bronze II] Title: Hashing, Time: 192 ms, Memory: 17688 KB, Score: 100 point -BaekjoonHub

--- Java/백준/Bronze/15829. Hashing/Hashing.java
+++ Java/백준/Bronze/15829. Hashing/Hashing.java
@@ -6,10 +6,14 @@ class Main {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
         String s = in.next();
-        int a = 0;
-        for (int i=0; i<N; i++){
-            int c = s.charAt(i) - 96;
-            a += c*Math.pow(31,i);
+        long a = 0;
+        long r = 1;
+        int mod = 1234567891;
+
+        for (int i = 0; i < N; i++) {
+            int c = s.charAt(i) - 'a' + 1;
+            a = (a + c * r) % mod;
+            r = (r * 31) % mod;
         }
         System.out.println(a);
     }
