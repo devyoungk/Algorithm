6fd19f9a467bfff45f1027249368a1d177424655
morelight <167386812+devyoungk@users.noreply.github.com>
2025-08-05 22:58:51 +0900

    [Bronze III] Title: ZOAC 4, Time: 184 ms, Memory: 17732 KB -BaekjoonHub

---
+++ Java/백준/Bronze/23971. ZOAC 4/ZOAC 4.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int H = sc.nextInt();
+		int W = sc.nextInt();
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+
+		int ans = ((W - 1) / (M + 1) + 1) * ((H - 1) / (N + 1) + 1);
+
+		System.out.println(ans);
+	}
+}
