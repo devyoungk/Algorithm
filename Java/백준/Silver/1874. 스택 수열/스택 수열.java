bfdddd6e1cebb80590c6e991ffc1589154ede7ad
TAEYOUNG <167386812+devyoungk@users.noreply.github.com>
2025-09-03 01:42:49 +0900

    [Silver II] Title: 스택 수열, Time: 844 ms, Memory: 111288 KB -BaekjoonHub

--- Java/백준/Silver/1874. 스택 수열/스택 수열.java
+++ Java/백준/Silver/1874. 스택 수열/스택 수열.java
@@ -7,29 +7,28 @@ public class Main {
 
 		int n = sc.nextInt();
 
-		Queue<Integer> Q = new ArrayDeque<>();
-		for (int i = 1; i <= n; i++) {
-			Q.add(i);
-		}
 		Stack<Integer> S = new Stack<>();
+		
+		int num = 1;
 
 		StringBuilder sb = new StringBuilder();
 
 		for (int i = 0; i < n; i++) {
 			int x = sc.nextInt();
-			while (!Q.isEmpty() && Q.peek() <= x) {
-				S.add(Q.poll());
-				sb.append('+').append("\n");
+			// 스택에 x 넣기
+			while (num <= x) { // x보다 작은 수를 아직 안넣었다면
+				S.push(num++); // x까지 계속 넣기
+				sb.append('+').append("\n"); // push연산 -> +
 			}
-			while (!S.isEmpty() && S.peek() == x) {
-				S.pop();
-				sb.append('-').append("\n");
+			if (S.peek() == x) { // 스택에 맨 위에 x가 있다면
+				S.pop(); // x 꺼내기
+				sb.append('-').append("\n"); // pop연산 -> -
 			}
-			if (!S.isEmpty() && S.peek() > x) {
-				System.out.println("NO");
-				return;
+			if (!S.isEmpty() && S.peek() > x) { // 스택 맨 위에 수가 x보다 크다면 -> 원하는 수열 만들 수 없음
+				System.out.println("NO"); // NO 출력 후
+				return; // 끝내기
 			}
 		}
-		System.out.println(sb);
+		System.out.println(sb); // 수열 완성되면 출력
 	}
 }
