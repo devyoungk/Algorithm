10a0fa6f4caee7411beb1f2f1b707da5eb97c115
TAEYOUNG <167386812+devyoungk@users.noreply.github.com>
2025-09-23 21:18:08 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1028 ms, Memory: 300628 KB -BaekjoonHub

--- Java/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ Java/백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -1,53 +1,50 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int[] checked; // 0: 방문 안함, 1: 방문 중(dfs), 2: 방문 완료
+	static int[] A;
+	static int n, cnt;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
 
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 0; t < T; t++) {
-			int N = Integer.parseInt(br.readLine());
-			int[] A = new int[N + 1];
+			n = Integer.parseInt(br.readLine());
+			A = new int[n + 1];
+			checked = new int[n + 1];
+
 			st = new StringTokenizer(br.readLine());
-			for (int i = 1; i <= N; i++) {
+			for (int i = 1; i <= n; i++) {
 				A[i] = Integer.parseInt(st.nextToken());
 			}
+			cnt = 0;
 
-			boolean[] checked = new boolean[N + 1];
-			boolean[] visited = new boolean[N + 1];
-
-			for (int i = 1; i <= N; i++) {
-				if (visited[i])
-					continue;
-				Set<Integer> currentPath = new HashSet<>();
-				int cur = i;
-				
-				while (true) {
-					if (visited[cur]) {
-						if (currentPath.contains(cur)) {
-							int cycleStartNode = cur;
-							while (!checked[cycleStartNode]) {
-								checked[cycleStartNode] = true;
-								cycleStartNode = A[cycleStartNode];
-							}
-						}
-						break;
-					}
-					visited[cur] = true;
-					currentPath.add(cur);
-					cur = A[cur];
+			for (int i = 1; i <= n; i++) {
+				if (checked[i] == 0) {
+					dfs(i);
 				}
 			}
-			
-			int cnt = 0;
-			for (int j = 1; j <= N; j++) {
-                if (!checked[j])
-                    cnt++;
-            }
-			System.out.println(cnt);
+			System.out.println(n - cnt);
 		}
+	}
+
+	static void dfs(int cur) {
+		checked[cur] = 1;
+		
+		int next = A[cur];
+		
+		if (checked[next] == 0) {
+			dfs(next);
+		} else if (checked[next] == 1) {
+			while(next != cur) {
+				cnt++;
+				next = A[next];
+			}
+			cnt++;
+		}
+		checked[cur] = 2;
 	}
 }
