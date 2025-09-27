61a4471367823e0e299bff13b1dc2fa7c1c9ccc7
TAEYOUNG <167386812+devyoungk@users.noreply.github.com>
2025-09-27 23:31:57 +0900

    [Silver III] Title: N과 M (4), Time: 128 ms, Memory: 16608 KB -BaekjoonHub

---
+++ Java/백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[] A;
+	static int N, M;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		A = new int[M];
+
+		backtracking(0);
+		
+		System.out.println(sb);
+	}
+
+	static void backtracking(int x) {
+		if (x == M) {
+			for (int a : A) {
+				sb.append(a).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for (int i = 1; i <= N; i++) {
+			if (x == 0 || A[x-1] <= i) {
+				A[x] = i;
+				backtracking(x + 1);
+			}
+		}
+	}
+}
