527455f55567dca0b2864b49df8791112540dca0
TAEYOUNG <167386812+devyoungk@users.noreply.github.com>
2025-09-28 22:01:42 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ Java/백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] A = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			A[i] = Integer.parseInt(br.readLine());
+		}
+
+		int[] dp = new int[N + 1];
+
+		dp[1] = A[1];
+		if (N > 1)
+			dp[2] = A[1] + A[2];
+
+		for (int i = 3; i <= N; i++) {
+			dp[i] = Math.max(dp[i - 3] + A[i - 1] + A[i], dp[i - 2] + A[i]);
+		}
+
+		System.out.println(dp[N]);
+	}
+
+}
