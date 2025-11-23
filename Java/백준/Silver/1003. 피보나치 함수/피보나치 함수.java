d4b1d9bbd4ce5eb4125bbec097944d1e40c30a7b
TAEYOUNG <167386812+devyoungk@users.noreply.github.com>
2025-11-23 15:10:28 +0900

    [Silver III] Title: 피보나치 함수, Time: 96 ms, Memory: 14112 KB -BaekjoonHub

--- Java/백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ Java/백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -3,25 +3,31 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int[] cnt0 = new int[41];
-		int[] cnt1 = new int[41];
-
-		cnt0[0] = 1;
-		cnt1[1] = 1;
-
-		for (int t = 1; t <= T; t++) {
-			int N = sc.nextInt();
-			if (cnt0[N] != 0 && cnt1[N] != 0) {
-				System.out.println(cnt0[N] + " " + cnt1[N]);
-			} else {
-				for (int i = 2; i <= N; i++) {
-					cnt0[i] = cnt0[i - 1] + cnt0[i - 2];
-					cnt1[i] = cnt1[i - 1] + cnt1[i - 2];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[][] dp = new int[41][2];
+		dp[0] = new int[] {1, 0};
+		dp[1] = new int[] {0, 1};
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		int idx = 2;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int t = 0; t < T; t++) {
+			int x = Integer.parseInt(br.readLine());
+			
+			if (x >= idx) {
+				for (int i = idx; i <= x; i++) {
+					dp[i][0] = dp[i-1][0] + dp[i-2][0];
+					dp[i][1] = dp[i-1][1] + dp[i-2][1];
 				}
-				System.out.println(cnt0[N] + " " + cnt1[N]);
+				idx = x + 1;
 			}
+			
+			sb.append(dp[x][0]).append(' ').append(dp[x][1]).append('\n');
 		}
+		System.out.println(sb);
 	}
 }
